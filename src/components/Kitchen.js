eba0839056b112b025f849ea4c15f1058765d851
chess2022 <lakeshore.hnh@gmail.com>
2022-04-30 18:08:03 -0700

    Adds Kitchen with Oven and Sink inside

--- src/components/Kitchen.js
+++ src/components/Kitchen.js
@@ -2,11 +2,11 @@ import React from "react";
 import Oven from "./Oven";
 import Sink from "./Sink";
 
-export default function Kitchen(props) {
+export default function Kitchen() {
   return (
     <div className="weather">
-      <Oven name={props.img} />
-      <Sink name={props.conditions} />
+      <Sink />
+      <Oven />
     </div>
   );
 }
